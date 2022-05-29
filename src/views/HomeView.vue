4125fb867bcab1e3d3190c6e7292d46f623c5fad
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2022-05-29 15:14:11 -0300

    Ajuste HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,48 +1,39 @@
 <template>
     <v-container>
         <div class="text-h5 mt-5 mb-12">Olá, mesa</div>
-        <v-row>
-            <navigation-button :properties="buttomNewOrder"> </navigation-button>
+        <v-row v-for="option in pathOptions" :key="option.path">
+            <NavigationButton :properties="option"> </NavigationButton>
         </v-row>
         <v-row>
-            <navigation-button :properties="buttomHelpCenter"> </navigation-button>
+            <CardSubmit :properties="cardSubmit"></CardSubmit>
         </v-row>
-        <card-submit :properties="cardSubmit"></card-submit>
     </v-container>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-
 import NavigationButton from '@/components/buttons/NavigationButton.vue';
 import CardSubmit from '@/components/cards/CardSubmit.vue';
 
 export default defineComponent({
     name: 'HomeView',
-
-    setup() {},
-
-    data() {
-        return {
-            cardSubmit: {
-                helpTitle: 'Quero ser atendido',
-                finishingTitle: 'Você logo será atendido!',
-                confirmationTitle: 'Confima a solicitação de ajuda?',
-            },
-            buttomNewOrder: {
+    data: () => ({
+        pathOptions: [
+            {
                 title: 'Novo Pedido',
                 path: '/cardapio',
             },
-            buttomHelpCenter: {
+            {
                 title: 'Central de Ajuda',
                 path: '/central-ajuda',
             },
-            buttomHelp: {
-                title: 'Quero ser atendido',
-                path: '/help',
-            },
-        };
-    },
+        ],
+        cardSubmit: {
+            helpTitle: 'Quero ser atendido',
+            finishingTitle: 'Você logo será atendido!',
+            confirmationTitle: 'Confima a solicitação de ajuda?',
+        },
+    }),
 
     components: {
         NavigationButton,
