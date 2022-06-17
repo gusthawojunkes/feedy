8c0b44e7de8da94c160ea70cd1b439e59f5773e3
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2022-06-16 22:07:24 -0300

    WIP

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -1,8 +1,13 @@
 <template>
     <v-container>
-        <div class="text-h5 mt-5 mb-12">Histórico de Pedidos</div>
-        <div v-if="orders.length <= 0">
+        <div class="text-h5 mt-5 mb-12">
+            <span>Histórico de Pedidos</span>
+        </div>
+        <div v-if="!loadingOrders && orders.length == 0">
             <v-alert prominent type="warning" variant="outlined"> Essa mesa não possui nenhum pedido ainda </v-alert>
+        </div>
+        <div v-else-if="loadingOrders" class="d-flex justify-center">
+            <v-progress-circular indeterminate color="#009688"></v-progress-circular>
         </div>
         <div v-else v-for="(order, index) in this.orders" :key="order.uid">
             <OrderDetail :order="order" :number="this.orders.length - index"></OrderDetail>
@@ -15,17 +20,20 @@ import { defineComponent } from 'vue';
 import OrderService from '@/services/order.service';
 import _ from 'lodash';
 import OrderDetail from '@/components/order/OrderDetail.vue';
-import Helper from '../utils/helper';
+import Helper from '@/utils/helper';
 
 export default defineComponent({
     name: 'OrdersList',
 
     async mounted() {
+        this.loadingOrders = true;
         await this.updateOrders();
+        this.loadingOrders = false;
     },
 
     data: () => ({
         orders: [],
+        loadingOrders: false,
     }),
 
     methods: {
