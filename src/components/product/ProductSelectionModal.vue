8369eb41643b0f7b756331a2d99986053136834d
Nathan <nathan.stuber.a@gmail.com>
2022-06-22 20:36:07 -0300

    wip

--- src/components/product/ProductSelectionModal.vue
+++ src/components/product/ProductSelectionModal.vue
@@ -1,29 +1,30 @@
 <template>
-    <v-dialog v-model="productSelection.dialog">
-        <v-card>
-            <v-card-title>
-                <span>
-                    {{ productSelection.item.product.name }}
-                </span>
+    <v-dialog v-model="productSelection.dialog" transition="dialog-top-transition">
+        <v-card width="500">
+            <v-toolbar color="#009688" dark prominent>
+                <v-toolbar-title>{{ productSelection.item.product.name }}</v-toolbar-title>
                 <v-spacer></v-spacer>
                 <v-btn icon @click="close()">
-                    <v-icon> mdi-close </v-icon>
+                    <v-icon>mdi-close</v-icon>
                 </v-btn>
-            </v-card-title>
-            <v-card-text>
+            </v-toolbar>
+            <v-card-text class="mt-8">
                 {{ productSelection.item.product.description }}
             </v-card-text>
-            <v-card-actions>
-                <v-btn icon color="#009688" @click="decrement()">
-                    <v-icon>mdi-minus</v-icon>
-                </v-btn>
-                <span>{{ productSelection.item.quantity }}</span>
-                <v-btn icon color="#009688" @click="increment()">
-                    <v-icon>mdi-plus</v-icon>
-                </v-btn>
-                <v-spacer></v-spacer>
-                <v-btn @click="addItem()">Adicionar</v-btn>
+            <v-card-actions class="mx-2 mb-1">
+                <v-card class="px-4">
+                    <v-btn icon outlined rounded color="#009688" @click="decrement()">
+                        <v-icon>mdi-minus</v-icon>
+                    </v-btn>
+                    <span>{{ productSelection.item.quantity }}</span>
+                    <v-btn icon color="#009688" @click="increment()">
+                        <v-icon>mdi-plus</v-icon>
+                    </v-btn>
+                </v-card>
+                <!-- <v-spacer></v-spacer> -->
+                <!-- <v-btn @click="close()" text>Cancelar</v-btn> -->
             </v-card-actions>
+            <v-btn @click="addItem()" outlined color="#009688" class="mx-4 mb-4">Adicionar</v-btn>
         </v-card>
     </v-dialog>
 </template>
